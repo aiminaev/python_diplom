0cfec9bbe6f4c6696e31c43c1a9c5e21c675d680
Андрей Минаев <cool.drey@yandex.ru>
2022-07-12 12:39:54 +0300

    feat: functionality update

---
+++ config.py
@@ -0,0 +1,10 @@
+user_token = ''  # токен пользователя
+comm_token = ''  # токен сообщества
+
+offset = 0
+line = range(0, 1000)
+
+host = 'localhost'  # адрес БД
+user = 'postgres'  # логин БД
+password = '1111'  # пароль БД
+db_name = 'vkinder'  # имя БД
